81b79e0471d00cd38f7af9cd1c9b6e694247ace5
kritkan118 <std.67122420118@ubru.ac.th>
2026-09-24 11:37:20 +0700

    ..

--- ubcf/__init__.py
+++ ubcf/__init__.py
@@ -5,9 +5,10 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ubcandle_db.sqlite'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ubcandle_db.sqlite'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/ubcandledb'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://<username>:<password><username>.mysql.pythonanywhere-services.com/<username>$<database_name>'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ubcf2026_06aw_user:NTUO64mQgC9nrazqXkPqZWjgFySi6zbM@dpg-daqaft0jo6nc73do5c1g-a.oregon-postgres.render.com/ubcf2026_06aw'
 app.config['SECRET_KEY'] = b'secretkey'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
